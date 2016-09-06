149247a6a802b9aded966303e565284445ec0983
David Gonzalez <nodearth@gmail.com>
2016-09-06 16:48:25 +0100

    added test. It is broken when running with lab... for now

--- test/hapi.test.js
+++ test/hapi.test.js
@@ -1,32 +1,58 @@
 'use strict'
 
-var Assert = require('assert')
-var Lab = require('lab')
-var Common = require('../lib/common')
+let expect = require('code').expect
+let Lab = require('lab')
+let Hapi = require('hapi')
+let Seneca = require('seneca')
+let Web = require('../')
+let request = require('request')
 
-var lab = exports.lab = Lab.script()
-var describe = lab.describe
-var it = lab.it
-var assert = Assert
+let lab = exports.lab = Lab.script()
 
-describe('hapi adapter', () => {
-  it('can autostart' (done) => {
-    done()
-  })
+let describe = lab.describe
+let it = lab.it
+let beforeEach = lab.beforeEach
+let afterEach = lab.afterEach
 
-  it('can accept a premade instance', (done) => {
-    done()
-  })
-
-  it('exports correctly', (done) => {
-    done()
-  })
-
-  it('passes settings correctly', (done) => {
-    done()
-  })
-
-  it('populates msg.args correctly', (done) => {
-
+let defaultRoutes = [
+  {
+    prefix: '/api',
+    pin: 'role:test,cmd:*',
+    map: {
+      ping: true
+    }
   }
+]
+
+
+var server = new Hapi.Server()
+server.connection({port: 4000})
+
+var seneca = Seneca()
+  .use(function plugin () {
+    this.add('role:test,cmd:ping', (msg, reply) => {
+      reply(null, {response: 'pong!'})
+    })
+  })
+  .use(Web, {adapter: 'hapi', context: server})
+  .ready(() => {
+    seneca.act('role:web', {routes: defaultRoutes}, (err, reply) => {
+      server.start((err) => {
+        console.log(server.table())
+        console.log('server started on: ' + server.info.uri)
+      })
+    })
+  })
+
+
+lab.experiment('hapi', () => {
+  
+  lab.test('can autostart', (done) => {
+  
+    request('http://127.0.0.1:4000/api/ping', (error, response, body) => {
+        expect(error).to.be.undefined
+        done()
+    })
+    
+  })
 })
