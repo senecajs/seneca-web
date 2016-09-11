39fbf2f18a997fe09fce2d43b54f1a6ac66064ff
Dean McDonnell <mcdonnelldean@outlook.com>
2016-09-11 20:59:13 +0100

    Hapi auth now working and documented - added example to show how to secure hapi routes - updated hapi adapter to support auth correctly

--- docs/examples/hapi-secure.js
+++ docs/examples/hapi-secure.js
@@ -2,12 +2,8 @@
 
 var Seneca = require('seneca')
 var Web = require('../../')
-var Express = require('express')
-var Passport = require('passport')
-var Strategy = require('passport-local').Strategy
-var CookieParser = require('cookie-parser')
-var BodyParser = require('body-parser')
-var Session = require('express-session')
+var Hapi = require('hapi')
+var Basic = require('hapi-auth-basic')
 var Repo = require('./common/repo')
 
 // The config for our routes
@@ -19,20 +15,17 @@ var Routes = [{
       POST: true,
       alias: '/'
     },
-    logout: {
-      GET: true,
-      redirect: '/'
-    },
     profile: {
       GET: true,
-      secure: {
+      auth: {
+        strategy: 'simple',
         fail: '/'
       }
     },
-    login: {
-      POST: true,
+    admin: {
+      GET: true,
       auth: {
-        strategy: 'local',
+        strategy: 'simple',
         pass: '/profile',
         fail: '/'
       }
@@ -48,75 +41,53 @@ function Plugin () {
     done(null, {ok: true, message: 'please log in...'})
   })
 
-  seneca.add('role:admin,cmd:logout', (msg, done) => {
-    msg.request$.logout()
-
-    done(null, {ok: true})
-  })
-
   seneca.add('role:admin,cmd:profile', (msg, done) => {
     done(null, {ok: true, user: msg.args.user})
   })
 }
 
-// Set our custom strategy in passport, plus user serialization.
-Passport.use(new Strategy((username, password, cb) => {
+var app = new Hapi.Server()
+app.connection({port: 4050})
+
+function validate (request, username, password, done) {
   Repo.users.findByUsername(username, (err, user) => {
     if (err) {
-      cb(err)
+      done(err)
     }
     else if (!user) {
-      cb(null, false)
+      done(null, false)
     }
     else if (user.password !== password) {
-      cb(null, false)
+      done(null, false)
     }
     else {
-      cb(null, user)
+      done(null, true, user)
     }
   })
-}))
+};
 
-Passport.serializeUser((user, cb) => {
-  cb(null, user.id)
-})
+app.register(Basic, (err) => {
+  if (err) throw err
 
-Passport.deserializeUser((id, cb) => {
-  Repo.users.findById(id, (err, user) => {
-    if (err) {
-      return cb(err)
-    }
-    else {
-      cb(null, user)
-    }
-  })
-})
+  app.auth.strategy('simple', 'basic', {validateFunc: validate})
 
-// Prep express
-var app = Express()
-app.use(CookieParser())
-app.use(BodyParser.urlencoded({extended: true}))
-app.use(Session({secret: 'magically', resave: false, saveUninitialized: false}))
-app.use(Passport.initialize())
-app.use(Passport.session())
+  // The config we will pass to seneca-web
+  var config = {
+    adapter: 'hapi',
+    context: app,
+    routes: Routes
+  }
 
-// The config we will pass to seneca-web
-var config = {
-  adapter: 'express',
-  context: app,
-  routes: Routes,
-  auth: Passport
-}
+  // Server and start as usual.
 
-// Server and start as usual.
+  var seneca = Seneca()
+    .use(Plugin)
+    .use(Web, config)
+    .ready(() => {
+      var server = seneca.export('web/context')()
 
-var seneca = Seneca()
-  .use(Plugin)
-  .use(Web, config)
-  .ready(() => {
-    var server = seneca.export('web/context')()
-
-    server.listen('4050', (err) => {
-      console.log(err || 'server started on: 4050')
+      server.start((err) => {
+        console.log(err || 'server started on: ' + server.info.uri)
+      })
     })
-  })
+})
